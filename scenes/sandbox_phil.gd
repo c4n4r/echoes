902138155118d58ab38eda790f2c5af190a40f1e
PhilTheZaz <113373668+PhilTheZaz@users.noreply.github.com>
2025-09-06 19:13:49 +0200

    Wip

--- scenes/sandbox_phil.gd
+++ scenes/sandbox_phil.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+@onready var player = $Present/Player
+
+func _ready() -> void:
+	var echo = Echo.new(
+		get_tree().current_scene.get_node("FrontMask") as ColorRect,
+		player,
+		25.0)
+	player.echo_instance = echo
+	add_child(echo)
+
+
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
